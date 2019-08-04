b07fe75b5d679da9f86adec77d8059f9cccf04c7
Admin <admin@localhost>
2019-08-04 01:50:51 -0400

    Used mmap to modify the file instead of reading it all into memory
    Added a main function

--- apng_thing.py
+++ apng_thing.py
@@ -1,27 +1,37 @@
 import binascii
+import mmap
 import struct
 
-apng_file = 'test 720p 15fps zopfli.png'
-new_delay = 25
 
-with open(apng_file, 'rb+') as f:
-    data = f.read()
-    f.seek(data.find(b'acTL') + 4)
-    header = struct.unpack('>II', f.read(8))
-    number_of_frames = header[0]  # Note that this includes the first frame even if skipped
-    last_position = 0
-    for i in range(number_of_frames - 1):
-        curr_position = data.find(b'fcTL', last_position)  # Find fcTL chunk
-        f.seek(curr_position)  # Go to chunk beginning
-        frame_info = list(struct.unpack('>ccccIIIIIHHBB', f.read(30)))  # Read in chunk data as a list
-        frame_info[10] = new_delay  # Set new delay denominator
+def main():
+    apng_file = 'test 720p 15fps zopfli.png'
+    new_delay = 15
 
-        # Generate CRC checksum from binary data
-        crc = binascii.crc32(struct.pack('>ccccIIIIIHHBB', *frame_info))
-        frame_info.append(crc)
+    with open(apng_file, 'rb+') as f:
+        with mmap.mmap(f.fileno(), 0) as m:
+            m.seek(m.find(b'acTL') + 4)
+            header = struct.unpack('>II', m.read(8))
+            number_of_frames = header[0]  # Note that this includes the first frame even if skipped
+            last_position = 0
 
-        f.seek(curr_position)  # Move back to chunk beginning
-        frame_binary = struct.pack('>ccccIIIIIHHBBI', *frame_info)  # Pack with CRC checksum
-        f.write(frame_binary)  # Write changes
+            for i in range(number_of_frames - 1):
+                curr_position = m.find(b'fcTL', last_position)  # Find fcTL chunk
+                m.seek(curr_position)  # Go to chunk beginning
+                frame_info = list(struct.unpack('>ccccIIIIIHHBB', m.read(30)))  # Read in chunk data as a list
+                frame_info[10] = new_delay  # Set new delay denominator
 
-        last_position = curr_position + 34  # Prevent the same chunk from being found
+                # Generate CRC checksum from binary data
+                crc = binascii.crc32(struct.pack('>ccccIIIIIHHBB', *frame_info))
+                frame_info.append(crc)
+
+                m.seek(curr_position)  # Move back to chunk beginning
+                frame_binary = struct.pack('>ccccIIIIIHHBBI', *frame_info)  # Pack with CRC checksum
+                m.write(frame_binary)  # Write changes
+
+                last_position = curr_position + 34  # Prevent the same chunk from being found
+
+            m.flush()
+
+
+if __name__ == '__main__':
+    main()
